859282260a00eb44cd2e74b36f38804cb6425a55
ccfz <casparfz@gmail.com>
2021-11-19 00:23:57 +0100

    Add dotenv to Project

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,6 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
-
+const dotenv = require('dotenv')
+dotenv.config()
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
