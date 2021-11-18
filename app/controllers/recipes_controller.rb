33cadb77627247e1c7e0837fda64fe56d8a9d250
ccfz <casparfz@gmail.com>
2021-11-19 00:27:40 +0100

    Increase number of max. visible recipe
    Currently, only 20 recipes are displayed. As we do not have more
    details specifications we should increase the number a little to
    mae sure enough recipes are displayed. In the future pagination
    would make the most sense. However, that is outside the scope of
    the PR.
    
    This changes the displayed recipes list from 20 to 100.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
   def index
-    recipes = Recipe.all.limit(20)
+    recipes = Recipe.all.limit(100)
 
     render json: recipes
   end
--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.describe "Recipes", type: :request do
   describe "GET /index" do
     it "returns an initial list of 20 recipes" do
-      create_list(:recipe, 100)
+      create_list(:recipe, 120)
 
       get '/recipes/index'
 
       parsed_body = JSON.parse(response.body)
-      expect(parsed_body.length).to eq(20)
+      expect(parsed_body.length).to eq(100)
     end
   end
 end
